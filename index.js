31fc4712a92f3a6e5c2c4b94e77de1297bd32ff8
BRAIAN GRACIA <braiansgracia@gmail.com>
2025-10-29 19:37:59 -0500

    arreglo de index

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const comicsController = require('./controllers/comics');
 const comicsRouter = require('./routes/comics');
 const abilityRouter = require('./routes/ability');
+const typeRouter = require('./routes/type');
+const speciesRouter = require('./routes/pokemon-species');
 const app = express();
 process.loadEnvFile()
 
@@ -11,6 +13,8 @@ app.use(express.json());
 
 app.use('/comics', comicsRouter);
 app.use('/ability', abilityRouter);
+app.use('/type', typeRouter); 
+app.use('/pokemon-species', speciesRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
